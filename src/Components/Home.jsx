1aad1c1cb8d297eb31d70b8d116ba51deac194d8
AnanthuSuresh098 <93374262+AnanthuSuresh098@users.noreply.github.com>
2023-10-06 18:40:30 +0530

    jsk home commit

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,31 +2,35 @@ import "./Home.css";
 import { useSpring, animated } from "react-spring";
 
 export const Home=()=>{
-    const [springProps, setSpring] = useSpring(() => ({
-      transform: "translateX(80%)",
-    }));
-
-      const handleScroll = () => {
-    const scrollY = window.scrollY;
-    const transformValue = `translateX(-${scrollY}px)`;
-    setSpring({ transform: transformValue });
-  };
-
-  window.addEventListener("scroll", handleScroll);
-
     return (
       <div id="homepage-main-wrapper">
         <div id="homepage-banner-section-wrap">
-          <animated.div
+          <div
             className="homepage-banner-section-image-wrap"
-            style={springProps}
           >
             <img
               src="./images/Home-img/banner.png"
               alt="banner"
               id="homepage-banner-section-image"
             />
-          </animated.div>
+          </div>
+
+          <div id="homepage-banner-section-content-wrap">
+            <div id="homepage-banner-section-content-title">
+              High Quality, Flexible Packagings From JSK Packagings
+            </div>
+            <div id="homepage-banner-section-content-desc">
+              We manufacture a wide range of products for various sectors
+            </div>
+            <div id="homepage-banner-section-button-wrap">
+              <button id="homepage-banner-section-contact-button">
+                Contact Us
+              </button>
+              <button id="homepage-banner-section-request-button">
+                Request For Free Sample
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     );
