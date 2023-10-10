304e97b65badc454a1edc7b0cb279d71040c4bb8
AnanthuSuresh098 <93374262+AnanthuSuresh098@users.noreply.github.com>
2023-10-10 18:31:07 +0530

    home commit

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,8 +2,15 @@ import "./Home.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
+import { useRef } from "react";
+import { IoIosArrowDropleftCircle } from "react-icons/io";
+import { IoIosArrowDroprightCircle } from "react-icons/io";
+import { BsArrowRightCircle } from "react-icons/bs";
 
 export const Home = () => {
+  
+  const slider01Ref = useRef(null);
+
   var slider01_settings = {
     className: "homepage-products-slider-component",
     dots: false,
@@ -13,9 +20,39 @@ export const Home = () => {
     autoplaySpeed: 1500,
     slidesToShow: 4,
     slidesToScroll: 1,
-    arrows: false,
     variableWidth: true,
+    arrows: true,
+    nextArrow: <Slider01NextArrow />,
+    prevArrow: <Slider01PrevArrow />,
   };
+
+    const handleSlider_01_NextClick = () => {
+      slider01Ref.current.slickNext();
+    };
+
+    function Slider01NextArrow() {
+      return (
+        <IoIosArrowDroprightCircle
+          className="home-slider01-nextarrow"
+          onClick={handleSlider_01_NextClick}
+        />
+      );
+    }
+
+    const handleSlider_01_PrevClick = () => {
+      slider01Ref.current.slickPrev();
+    };
+
+    function Slider01PrevArrow() {
+      return (
+        <IoIosArrowDropleftCircle
+          className="home-slider01-prevarrow"
+          onClick={handleSlider_01_PrevClick}
+        />
+      );
+    }
+
+
 
   return (
     <div id="homepage-main-wrapper">
@@ -51,7 +88,7 @@ export const Home = () => {
           Our <span>Products</span>
         </div>
         <div id="homepage-products-slider-main-wrap">
-          <Slider {...slider01_settings}>
+          <Slider ref={slider01Ref} {...slider01_settings}>
             <div className="homepage-products-slider-slides">
               <div className="homepage-products-slider-image-wrap">
                 <img src="./images/Home-img/lidrolls.png" alt="" />
@@ -149,6 +186,96 @@ export const Home = () => {
               </div>
             </div>
           </Slider>
+        </div>
+      </div>
+
+      <div id="our-markets-section-main-wrap">
+        <div id="our-markets-section-images-wrap">
+          <div>
+            <img src="./images/Home-img/markets.png" alt="" />
+          </div>
+          <div>
+            <img src="./images/Home-img/spout.png" alt="" />
+          </div>
+          <div>
+            <img src="./images/Home-img/markets.png" alt="" />
+          </div>
+          <div>
+            <img src="./images/Home-img/stretch.png" alt="" />
+          </div>
+          <div>
+            <img src="./images/Home-img/markets.png" alt="" />
+          </div>
+          <div>
+            <img src="./images/Home-img/markets01.jpeg" alt="" />
+          </div>
+        </div>
+
+        <div id="our-markets-section-content-wrap">
+          <div id="our-markets-section-content-headline">
+            Our <span>Markets</span>
+          </div>
+          <div>
+            For food manufacturers around the world, we provide a wide selection
+            of specialised lidding films and flexible packaging solutions.{" "}
+          </div>
+          <div>
+            We cater to all marketplaces, including those for prepared foods,
+            fresh produce, confections, and snacks.
+          </div>
+          <div id="our-markets-section-button">
+            Know More
+            <BsArrowRightCircle id="our-markets-section-button-icon" />
+          </div>
+        </div>
+      </div>
+
+      <div id="homepage-about-section-main-wrap">
+        <div id="homepage-about-section-content-wrap">
+          <div id="homepage-about-section-content-headline">
+            About <span>Us</span>
+          </div>
+          <div>
+            Since its founding in 1999, JSK PKG has been a forerunner in
+            flexible packaging. Our modern manufacturing facility is in the
+            heart of Hyderabad, near Shamshabad Airport.
+          </div>
+          <div>
+            Our specialties include high-quality laminated pouches and
+            multilayer laminates; we produce them for various markets.
+          </div>
+          <div id="homepage-about-section-button">
+            Know More
+            <BsArrowRightCircle id="homepage-about-section-button-icon" />
+          </div>
+        </div>
+
+        <div id="homepage-about-section-image-wrap">
+          <div>
+            <img src="./images/Home-img/home-about.jpeg" alt="" />
+          </div>
+          <div>
+            <img src="./images/Home-img/home-about01.png" alt="" />
+          </div>
+        </div>
+      </div>
+
+      <div id="homepage-free-sample-section-wrap">
+        <div id="homepage-free-sample-section-image-wrap">
+          <img src="./images/Home-img/objects.png" alt="" />
+        </div>
+        <div id="homepage-free-sample-section-content-wrap">
+          <div id="homepage-free-sample-section-content-title">
+            Get your free sample
+          </div>
+          <div id="homepage-free-sample-section-content-description">
+            Share your specific needs, and our dedicated team will reach out to
+            you promptly!
+          </div>
+          <div id="homepage-free-sample-section-button">
+            Know More
+            <BsArrowRightCircle id="homepage-free-sample-section-button-icon" />
+          </div>
         </div>
       </div>
     </div>
