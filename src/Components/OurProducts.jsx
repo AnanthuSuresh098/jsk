8e059105a0c14c2b55f1a7e95c63c25b74b6495b
AnanthuSuresh098 <93374262+AnanthuSuresh098@users.noreply.github.com>
2023-10-11 19:03:26 +0530

    products page commit

---
+++ src/Components/OurProducts.jsx
@@ -0,0 +1,28 @@
+import "./OurProducts.css";
+
+export const OurProducts=()=>{
+    return (
+      <div id="our-products-page-main-wrapper">
+        <div id="our-products-page-headline">
+          Our <span>Products</span>
+        </div>
+        <div id="our-products-page-description">
+          Our JSK range offers packaging products which are broadly appreciated
+          by our esteemed clients for their light weight, recyclable nature,
+          fine finish, high stiffness and moist proof nature. These packing
+          products are also available in several customizations to meet the
+          specific demands of our clients.
+        </div>
+        <div id="our-products-page-grid-main-wrap">
+          <div>
+            <img src="" alt="" />
+            <div>Sealing Lid Rolls</div>
+          </div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    );
+}
--- src/Routes/MainRoutes.jsx
+++ src/Routes/MainRoutes.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import { Home } from "../Components/Home.jsx";
 import { Navbar } from "../Components/Navbar.jsx";
+import { OurProducts } from "../Components/OurProducts.jsx";
 
 export const MainRoutes = () =>{
     return (
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/ourproducts" element={<OurProducts />} />
       </Routes>
     );
 }
